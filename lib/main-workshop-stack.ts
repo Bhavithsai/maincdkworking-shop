eb834049717b35af10b2b0bc82284a3255402587
EC2 Default User <ec2-user@ip-172-31-32-89.ap-northeast-1.compute.internal>
2024-02-23 18:13:32 +0000

    first

--- lib/main-workshop-stack.ts
+++ lib/main-workshop-stack.ts
@@ -1,19 +1,38 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+// import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
+
 
 export class MainWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MainWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+  const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    }); 
+  // new apigw.LambdaRestApi(this, 'Endpoint', {
+  //     handler: hello
+  //   });
+  const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
-    const topic = new sns.Topic(this, 'MainWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+    
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
   }
 }
